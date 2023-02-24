ee2ee063707ff9bd9f47dbeab6caa474a98f88a6
lorcancooke <lorcancooke@gmail.com>
2023-02-24 20:26:23 +0000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom'
+
+it ('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I was changed/i);
   expect(linkElement).toBeInTheDocument();
 });
